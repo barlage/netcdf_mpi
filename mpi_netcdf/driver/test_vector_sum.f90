ebb5709cee9f907da84a357bf2c1e5347f91cac6
Michael Barlage <michael.barlage@noaa.gov>
2025-03-28 11:15:21 -0600

    test

--- mpi_netcdf/driver/test_vector_sum.f90
+++ mpi_netcdf/driver/test_vector_sum.f90
@@ -69,7 +69,7 @@ end program
   subroutine handle_err(status, message)
     use netcdf
     integer, intent ( in) :: status
-    character*100, intent ( in), optional :: message
+    character(len=*), intent ( in), optional :: message
  
     if(status /= nf90_noerr) then
       print *, trim(nf90_strerror(status))
